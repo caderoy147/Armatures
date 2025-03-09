2157e0d3779ca8c2468a10c4124689640238b6eb
caderoy147 <benedictjamesenj@gmail.com>
2025-03-09 10:11:31 +0800

    sysRs

--- servoControll/ServoControl.h
+++ servoControll/ServoControl.h
@@ -1,3 +1,31 @@
+/*
+ * ServoControl.h & ServoControl.cpp
+ * 
+ * Servo Control Library for Arduino
+ * 
+ * Authors: Arbasto, Nathaniel L., Enjambre, Benedict James M., Punay, Jayco Belle C.,
+ * Created: 09/03/2025
+ * License: MIT License
+ * 
+ * Description:
+ * This library provides functions to control up to MAX_SERVOS servos using 
+ * smooth motion and speed control. It includes initialization, speed adjustment, 
+ * and movement functions, including special handling for a claw mechanism.
+ * 
+ * Features:
+ * - Supports up to 6 servos.
+ * - Smooth movement with configurable speed.
+ * - Claw control with adjustable grip width.
+ * 
+ * Usage:
+ * - Call `initializeServos()` to set up servos.
+ * - Use `moveServoSmooth(index, target)` to move a servo gradually.
+ * - Adjust speed with `setSpeed(index, speed)`.
+ * - Control the claw using `controlClaw(status, width)`.
+ * 
+ * (c) 2025 Armatures. Released under the MIT License.
+ */
+
 #ifndef SERVO_CONTROL_H
 #define SERVO_CONTROL_H
 
